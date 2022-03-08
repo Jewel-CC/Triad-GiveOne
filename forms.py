0a725801d817e82cc046b9486bcd02a88c7d25dc
adriank112 <61247218+adriank112@users.noreply.github.com>
2022-03-07 19:27:17 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,48 +1,30 @@
+from html.entities import html5
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,TextAreaField
-from wtforms.validators import InputRequired, EqualTo
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField, PasswordField, SubmitField,TextAreaField, SelectField, RadioField, EmailField, validators, ValidationError
+from wtforms.validators import InputRequired, EqualTo, Email
+
+
 
 class SignUp(FlaskForm):
     username = StringField('Username', validators=[InputRequired()],render_kw={'class': 'white-text'})
-    email = EmailField('Email', validators=[InputRequired()])
-    password = PasswordField('Enter Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    # email = EmailField('Email', validators=[Email(),InputRequired()])
+    password = PasswordField('Enter Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords do not match')])
     confirm  = PasswordField('Confirm Password')
     submit = SubmitField('Create Account', render_kw={'class': 'btn black-text red accent-4'})
 
 class Login(FlaskForm):
     username = StringField('Username', validators=[InputRequired()],render_kw={'class': 'white-text'})
-    password = PasswordField('Enter Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    password = PasswordField('Enter Password', validators=[InputRequired()])
     submit = SubmitField('Login', render_kw={'class': 'btn black-text red accent-4'})
 
-class NewTopic(FlaskForm):
-	title = StringField('Title', validators=[InputRequired()],render_kw={'class': 'white-text'})
-	content =TextAreaField('New Post....',validators =[InputRequired()])
-	submit = SubmitField('Publish', render_kw={'class': 'btn black-text red accent-4'})
+class newRequest(FlaskForm): 
+    title = StringField('Title', validators=[InputRequired()],render_kw={'class': 'white-text'})
+    desription =TextAreaField('Your request in 200 characters or less....',validators =[InputRequired()])
+    category  = SelectField('Please choose the most appropriate tags to help identify your request', choices=[('Food'), ('School'), ('Medicine'), ('Furniture'), ('Clothing'), ('Custom')])
+    item = StringField('Item', validators=[InputRequired()]) #List
+    urgent = RadioField('Is this request urgent?', choices=[('No'), ('Yes')])
+    submit = SubmitField('Publish', render_kw={'class': 'btn black-text red accent-4'})
 
-class NewPost(FlaskForm):
-	content =TextAreaField('New Post....',validators =[InputRequired()])
-	submit = SubmitField('Post', render_kw={'class': 'btn black-text red accent-4'})
-	topic = StringField()
-
-
-class NewWorkout(FlaskForm):
-	Title = StringField('Workout Name')
-	Excercise1 = StringField('Excercise')
-	Excercise2 = StringField('Excercise')
-	Excercise3 = StringField('Excercise')
-	Excercise4 = StringField('Excercise')
-	Excercise5 = StringField('Excercise')
-	Reps1 = StringField('Reps')
-	Reps2 = StringField('Reps')
-	Reps3 = StringField('Reps')
-	Reps4 = StringField('Reps')
-	Reps5 = StringField('Reps')
-	Sets1 = StringField('Sets')
-	Sets2 = StringField('Sets')
-	Sets3 = StringField('Sets')
-	Sets4 = StringField('Sets')
-	Sets5 = StringField('Sets')
-	submit = SubmitField('Create', render_kw={'class': 'btn black-text red accent-4'})
-	
-
+class newDonation(FlaskForm):
+    items = SelectField('', choices=[('Food')])
+  
