a81b3821a72f7f5bb3db493fc52c8026ee086bde
AaronQuestel <73033823+AaronQuestel@users.noreply.github.com>
2022-03-25 18:22:39 -0400

    Authentication setup and requirements redone

--- app.py
+++ app.py
@@ -1,77 +1,98 @@
-import json
-from flask import Flask,request,redirect,url_for,flash
-from flask import render_template
-from forms import SignUp,LogIn,newRequest,newDonation
-from models import db,User, Item, Giver,Getter,Request
-from flask_login import LoginManager, current_user, login_user,logout_user,login_required
+from flask import Flask, request, jsonify, render_template, redirect, flash, url_for, jsonify
+from flask_cors import CORS
+from flask_login import LoginManager, current_user, login_user, login_required
 from sqlalchemy.exc import IntegrityError
 
+from models import db, User, Requests, Donations
 
+''' Begin Flask Login Functions '''
+login_manager = LoginManager()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "SECRET"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
-db.init_app(app)
-# login_manager.init_app(app) 
+''' End Flask Login Functions '''
+
+''' Begin boilerplate code '''
+def create_app():
+  app = Flask(__name__, static_url_path='')
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+  app.config['SECRET_KEY'] = "MYSECRET"
+  CORS(app)
+  login_manager.init_app(app) # uncomment if using flask login
+  db.init_app(app)
+  db.create_all(app=app)
+  return app
+
+app = create_app()
+
 app.app_context().push()
-db.create_all(app=app)
+
+''' End Boilerplate Code '''
 
 @app.route('/', methods=['GET'])
 def index():
-  form = LogIn()
-  return render_template('login.html', form=form)
+  return render_template('signup.html')
 
-@app.route('/login', methods=['POST'])
-def loginAction():
-  form = LogIn()
-  if form.validate_on_submit(): # respond to form submission
-      data = request.form
-      user = User.query.filter_by(username = data['username']).first()
-      if user and user.check_password(data['password']): # check credentials
-        flash('Logged in successfully.') # send message to next page
-        login_user(user) # login the user
-        return redirect(url_for('all_requests')) # redirect to main page if login successful
-  flash('Invalid credentials')
-  return redirect(url_for('index'))
-
-
+# SIGNUP FUNCTIONALITY
 @app.route('/signup', methods=['GET'])
 def signup():
-  form = SignUp() # create form object
-  return render_template('signup.html', form=form) # pass form object to template
+  return render_template('signup.html')
 
 @app.route('/signup', methods=['POST'])
 def signupAction():
-  form = SignUp() # create form object
-  if form.validate_on_submit():
-    data = request.form # get data from form submission
-    newuser = User(username=data['username'], email=data['email']) # create user object
-    newuser.set_password(data['password']) # set password
-    db.session.add(newuser) # save new user
-    db.session.commit()
+  data = request.form # get data from form submission
+  newuser = User(username=data['username'], email=data['email']) # create user object
+  newuser.set_password(data['password']) # set password
+  try:
+    db.session.add(newuser)
+    db.session.commit() # save user
+    login_user(newuser) # login the user
     flash('Account Created!')# send message
-    return redirect(url_for('index'))# redirect to login page
-  flash('Error invalid input!')
-  return redirect(url_for('signup'))  
+    return redirect(url_for('all_requests'))# redirect to homepage
+  except IntegrityError: # attempted to insert a duplicate user
+    db.session.rollback()
+    flash("username or email already exists") # error message
+  return redirect(url_for('signup'))
 
+
+# LOGIN FUNCTIONALITY
+@app.route('/login', methods=['GET'])
+def login():
+  return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def loginAction():
+  data = request.form
+  user = User.query.filter_by(username = data['username']).first()
+  if user and user.check_password(data['password']): # check credentials
+    flash('Logged in successfully.') # send message to next page
+    login_user(user) # login the user
+    return redirect(url_for('all_requests')) # redirect to main page if login successful
+  else:
+    flash('Invalid username or password') # send message to next page
+  return render_template('login.html')
+
+
+# REQUESTS FUNCTIONALITY
 @app.route('/all_requests', methods=['GET'])
+@login_required
 def all_requests():
-  return render_template('all_requests2.html') 
+  return render_template('all_requests.html') 
 
+
+# DONATIONS FUNCTIONALITY
 @app.route('/all_donations', methods=['GET'])
+@login_required
 def all_donations():
   return render_template('all_donations.html') 
 
-@app.route('/donation_page', methods=['GET'])
-def donation_page():
-  return render_template('donation_page.html') 
-
-@app.route('/request_page', methods=['GET'])
-def request_page():
-  return render_template('request_page.html') 
-
+@app.route('/about_us', methods=['GET'])
+@login_required
+def about_us():
+  return render_template('about_us.html') 
 
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+  app.run(host='0.0.0.0', port=8080, debug=True)
